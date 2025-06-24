d1020bfee74631a7d91a62b2a87a5e4d8aec02e3
seizenB <seizenb@gmail.com>
2025-06-25 06:34:29 +0800

    `fix`: Memperbaiki konflik

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
@@ -46,53 +45,3 @@ def role_checker(required_roles: list[str]):
                 detail="You don't have enough permissions"
             )
         return current_user
-=======
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-from app.core.config import settings
-from database import get_db
-from app.models import User as user_models
-from app.crud import user as users_crud
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-
-class TokenData(BaseModel):
-    email: str | None = None
-
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-        token_data = TokenData(email=email)
-    except JWTError:
-        raise credentials_exception
-    user = users_crud.get_user_by_email(db, email=token_data.email)
-    if user is None:
-        raise credentials_exception
-    return user
-
-def get_current_active_user(current_user: user_models.User = Depends(get_current_user)):
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-def role_checker(required_roles: list[str]):
-    def checker(current_user: user_models.User = Depends(get_current_active_user)):
-        if current_user.role not in required_roles:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="You don't have enough permissions"
-            )
-        return current_user
->>>>>>> ec7fc7c (Commit perubahan lokal sebelum merge)
-    return checker
